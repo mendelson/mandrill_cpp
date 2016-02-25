2c4133e3994bb3e6e7e9a887d79689875dfe3175
mendelson <m.mendelson.unb@gmail.com>
2016-02-25 17:29:52 -0300

    Makefile fixed again

--- scripts/compileAll.sh
+++ scripts/compileAll.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 ####################### DEPRECATED #######################
+# Use makefile instead
 
 mkdir -p ../bin
 
-g++ -std=c++0x -pthread -c -I ../source/header ../source/src/main.cpp ../source/src/Camera.cpp ../source/src/CameraStandards.cpp ../source/src/FramesManager.cpp ../source/src/Analyser.cpp `pkg-config --cflags opencv` -o ../bin/main `pkg-config --libs opencv` -O4
+g++ -std=c++0x -pthread -I ../source/header ../source/src/main.cpp ../source/src/Camera.cpp ../source/src/FramesManager.cpp ../source/src/Analyser.cpp ../source/src/Printer.cpp `pkg-config --cflags opencv` -o ../bin/main `pkg-config --libs opencv` -O4
 
 # g++ -std=c++0x ../src/shared_ptr_test.cpp `pkg-config --cflags opencv` -o ../bin/main `pkg-config --libs opencv` -O4
