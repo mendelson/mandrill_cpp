0e56e8df67d31acda20d49f3aadcc3f72d89fde5
okbaby <maciel.lucas@outlook.com>
2016-05-30 18:34:50 -0300

    Fixed Merge THe previous merge was pushed without the changes at MoementProcessor header

--- source/header/MovementProcessor.hpp
+++ source/header/MovementProcessor.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <unordered_map>
 #include "Processor.hpp"
 #include <ctime>
+
+typedef std::unordered_map<unsigned int, std::shared_ptr<cv::Mat>> FramesSet;
+
 
 class MovementProcessor : public Processor
 {
@@ -17,9 +21,15 @@ public:
 private:
   cv::Mat _greyFrame;
   cv::Mat _greyFrame2;
+  cv::Mat _storedFrame;
   cv::VideoWriter* _outputStream;
   unsigned int _storedFrameIndex;
-  cv::Mat _storedFrame;
-  void saveFrame(cv::Mat frame);
+  unsigned int _latestFrameIndex;
+  unsigned int _lastFrame;
+  unsigned int _firstFrame;
   std::string _codecName;
+  FramesSet _cameraBuffer;
+  void saveFrame(cv::Mat frame);
+  void addFrame(cv::Mat frame);
+
 };
